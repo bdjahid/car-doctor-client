2119a0f6e49cac832ac5aba345d161243a711d00
Your Name <you@example.com>
2023-12-07 12:58:11 +0600

    about

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from './../layout/Main';
 import Home from "../pages/Home/Home/Home";
+import About from "../pages/Home/About/About";
 
 const router = createBrowserRouter([
     {
@@ -12,7 +13,8 @@ const router = createBrowserRouter([
                 element: <Home></Home>
             },
             {
-
+                path: "/about",
+                element: <About></About>
             }
         ]
     },
