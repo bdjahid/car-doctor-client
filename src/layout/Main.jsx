b1be26ca51cf896fc22f206a67c8ee5f4a5a0d31
Your Name <you@example.com>
2023-12-07 12:37:42 +0600

    banner

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,11 +1,15 @@
 import { Outlet } from "react-router-dom";
+import Footer from "../shared/Footer/Footer";
+import Navber from "../shared/Navber/Navber";
 
 
 
 const Main = () => {
     return (
         <div>
+            <Navber></Navber>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
