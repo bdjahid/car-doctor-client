6aa1104765f289a75a076c1d49f1e364eea43d36
Your Name <you@example.com>
2023-12-09 18:12:08 +0600

    delete

--- src/pages/Bookings/BookingRow.jsx
+++ src/pages/Bookings/BookingRow.jsx
@@ -1,13 +1,29 @@
 
 
 const BookingRow = ({ booking }) => {
-    const { date, service, price, img } = booking
+    const { _id, date, services, price, img } = booking;
+
+    const handleDelete = id => {
+        const proceed = confirm('Are you sure you want to delete')
+        if (proceed) {
+            fetch(`http://localhost:5000/bookings/${id}`, {
+                method: "DELETE"
+            })
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data)
+                    if (data.deletedCount > 0) {
+                        alert('deleted successfully')
+                    }
+                })
+        }
+    }
     return (
         <tr>
             <th>
-                <label>
-                    <input type="checkbox" className="checkbox" />
-                </label>
+                <button onClick={() => handleDelete(_id)} className="btn btn-circle">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
+                </button>
             </th>
             <td>
                 <div className="avatar">
@@ -17,7 +33,7 @@ const BookingRow = ({ booking }) => {
                 </div>
             </td>
             <td>
-                {service}
+                {services}
             </td>
             <td>{date}</td>
             <td>${price}</td>
--- src/pages/Bookings/Bookings.jsx
+++ src/pages/Bookings/Bookings.jsx
@@ -1,6 +1,7 @@
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../Providers/AuthProvider";
 import BookingRow from "./BookingRow";
+import Services from './../Home/Services/Services';
 
 
 
@@ -15,6 +16,24 @@ const Bookings = () => {
             .then(data => setBookings(data))
     })
 
+    const handleDelete = id => {
+        const proceed = confirm('Are you sure you want to delete')
+        if (proceed) {
+            fetch(`http://localhost:5000/bookings/${id}`, {
+                method: "DELETE"
+            })
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data)
+                    if (data.deletedCount > 0) {
+                        alert('deleted successfully')
+                        const remaining = bookings.filter(booking => booking._id !== id);
+                        setBookings(remaining)
+                    }
+                })
+        }
+    }
+
     return (
         <div className="overflow-x-auto">
             <table className="table">
@@ -26,10 +45,11 @@ const Bookings = () => {
                                 <input type="checkbox" className="checkbox" />
                             </label>
                         </th>
-                        <th>Name</th>
-                        <th>Job</th>
-                        <th>Favorite Color</th>
-                        <th></th>
+                        <th>Image</th>
+                        <th>Service</th>
+                        <th>Date</th>
+                        <th>Price</th>
+                        <th>Status</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -37,6 +57,7 @@ const Bookings = () => {
                         bookings.map(booking => <BookingRow
                             key={booking._id}
                             booking={booking}
+                            handleDelete={handleDelete}
                         ></BookingRow>)
                     }
 
